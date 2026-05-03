fe442f2897d9b8d8f4bb3f3ce92893685f09c8f9
fancykejriwal <ms.fkejriwal@outlook.com>
2026-05-03 19:35:11 +0530

    final

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
-
 from flask import Flask, render_template, request, redirect, url_for
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-import certifi
+import os
 
 app = Flask(__name__)
 
-
-app.config["MONGO_URI"] = "mongodb+srv://FANCYKEJRIWAL:F%4015051996cy@cluster0.xxxxx.mongodb.net/testdb?retryWrites=true&w=majority"
-
-# Secret key (can be anything for now)
+# Secret key
 app.secret_key = "mysecretkey"
 
-# Initialize MongoDB
-mongo = PyMongo(app, tlsCAFile=certifi.where())
+# Detect testing mode
+TESTING = os.getenv("TESTING", "False") == "True"
+
+# Only connect Mongo if NOT testing
+if not TESTING:
+    from flask_pymongo import PyMongo
+    from bson.objectid import ObjectId
+    import certifi
+
+    app.config["MONGO_URI"] = os.getenv(
+        "MONGO_URI",
+        "mongodb+srv://FANCYKEJRIWAL:F%4015051996cy@cluster0.xxxxx.mongodb.net/testdb?retryWrites=true&w=majority"
+    )
+
+    mongo = PyMongo(app, tlsCAFile=certifi.where())
+else:
+    mongo = None
 
 
 # ---------------- ROUTES ---------------- #
 
-# Home page -> list students
+# Home page
 @app.route('/')
 def index():
-    students = mongo.db.students.find()
-    return render_template('index.html', students=students)
+    if mongo:
+        students = mongo.db.students.find()
+        return render_template('index.html', students=students)
+    else:
+        return "Hello, Jenkins CI/CD!"
 
 
 # Add student
 @app.route('/add', methods=['GET', 'POST'])
 def add_student():
+    if not mongo:
+        return "DB not available in CI"
+
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
@@ -47,22 +62,20 @@ def add_student():
 # Update student
 @app.route('/update/<student_id>', methods=['GET', 'POST'])
 def update_student(student_id):
+    if not mongo:
+        return "DB not available in CI"
+
     student = mongo.db.students.find_one({"_id": ObjectId(student_id)})
 
     if request.method == 'POST':
-        new_name = request.form['name']
-        new_email = request.form['email']
-        new_course = request.form['course']
-
         mongo.db.students.update_one(
             {"_id": ObjectId(student_id)},
             {"$set": {
-                "name": new_name,
-                "email": new_email,
-                "course": new_course
+                "name": request.form['name'],
+                "email": request.form['email'],
+                "course": request.form['course']
             }}
         )
-
         return redirect(url_for('index'))
 
     return render_template('update_student.html', student=student)
@@ -71,6 +84,9 @@ def update_student(student_id):
 # Delete student
 @app.route('/delete/<student_id>')
 def delete_student(student_id):
+    if not mongo:
+        return "DB not available in CI"
+
     mongo.db.students.delete_one({"_id": ObjectId(student_id)})
     return redirect(url_for('index'))
 
